dc884bdde93dd042453bc25820028ddbd92b9a01
J-Django-Github <2458186212@qq.com>
2023-08-24 01:48:12 +0800

    1、更新图片

--- src/pages/jknwzy/index.vue
+++ src/pages/jknwzy/index.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="body">
-        <div class="header">
-            <div class="logo">
+        <div class="header" :class="[scrollTop > 50 ? 'scroll' : '']">
+            <span class="title">
+                jkn
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="32" height="32" viewBox="0 0 24 24">
                     <path fill="currentColor"
                         d="M12 20.325q-.35 0-.713-.125t-.637-.4l-1.725-1.575q-2.65-2.425-4.788-4.813T2 8.15Q2 5.8 3.575 4.225T7.5 2.65q1.325 0 2.5.562t2 1.538q.825-.975 2-1.538t2.5-.562q2.35 0 3.925 1.575T22 8.15q0 2.875-2.125 5.275T15.05 18.25l-1.7 1.55q-.275.275-.637.4t-.713.125Z" />
                 </svg>
-            </div>
-            <span class="title">
-                JKN-WZY
+                wzy
             </span>
             <span class="theme-box">
                 主题模式：
@@ -19,7 +18,7 @@
             <template v-for="(item, index) of config.imgs" :key="index">
                 <div class="item">
                     <span class="index">
-                        编号：{{ index + 1 }}
+                        编号: {{ index + 1 }}
                     </span>
                     <img class="img" :src="item" />
                 </div>
@@ -29,18 +28,17 @@
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
-import { useAppStore } from '@/store/modules/app'
+import { ref, reactive, onMounted, onUnmounted } from 'vue'
 import ThemeSwitch from '@/components/theme.vue'
-import Z1 from '@/assets/images/z.jpg'
-import Z2 from '@/assets/images/z1.jpg'
-import Z3 from '@/assets/images/z3.jpg'
-import Z4 from '@/assets/images/z4.jpg'
-import Z5 from '@/assets/images/z5.jpg'
-import Z6 from '@/assets/images/z6.jpg'
-import Z7 from '@/assets/images/z7.jpg'
-import Z8 from '@/assets/images/z8.jpg'
-import Z9 from '@/assets/images/z9.jpg'
+import Z01 from '@/assets/images/z.jpg'
+import Z02 from '@/assets/images/z1.jpg'
+import Z03 from '@/assets/images/z3.jpg'
+import Z04 from '@/assets/images/z4.jpg'
+import Z05 from '@/assets/images/z5.jpg'
+import Z06 from '@/assets/images/z6.jpg'
+
+import Z08 from '@/assets/images/z8.jpg'
+import Z09 from '@/assets/images/z9.jpg'
 import Z10 from '@/assets/images/z10.jpg'
 import Z11 from '@/assets/images/z11.jpg'
 import Z12 from '@/assets/images/z12.jpg'
@@ -49,7 +47,7 @@ import Z14 from '@/assets/images/z14.jpg'
 import Z15 from '@/assets/images/z15.jpg'
 import Z16 from '@/assets/images/z16.jpg'
 import Z17 from '@/assets/images/z17.jpg'
-import Z18 from '@/assets/images/z18.jpg'
+
 import Z19 from '@/assets/images/z19.jpg'
 import Z20 from '@/assets/images/z20.jpg'
 import Z21 from '@/assets/images/z21.jpg'
@@ -57,7 +55,7 @@ import Z22 from '@/assets/images/z22.jpg'
 import Z23 from '@/assets/images/z23.jpg'
 import Z24 from '@/assets/images/z24.jpg'
 import Z25 from '@/assets/images/z25.jpg'
-import Z26 from '@/assets/images/z26.jpg'
+
 import Z27 from '@/assets/images/z27.jpg'
 import Z28 from '@/assets/images/z28.jpg'
 import Z29 from '@/assets/images/z29.jpg'
@@ -115,19 +113,43 @@ import Z80 from '@/assets/images/z80.jpg'
 import Z81 from '@/assets/images/z81.jpg'
 import Z82 from '@/assets/images/z82.jpg'
 import Z83 from '@/assets/images/z83.jpg'
+import Z84 from '@/assets/images/z84.jpg'
+import Z85 from '@/assets/images/z85.jpg'
+import Z86 from '@/assets/images/z86.jpg'
+import Z87 from '@/assets/images/z87.jpg'
+import Z88 from '@/assets/images/z88.jpg'
+import Z89 from '@/assets/images/z89.jpg'
+import Z90 from '@/assets/images/z90.jpg'
+import Z91 from '@/assets/images/z91.jpg'
+import Z92 from '@/assets/images/z92.jpg'
+import Z93 from '@/assets/images/z93.jpg'
+import Z94 from '@/assets/images/z94.jpg'
+import Z95 from '@/assets/images/z95.jpg'
+import Z96 from '@/assets/images/z96.jpg'
 
-const appStore = useAppStore();
+const scrollTop = ref<number>(0);
 
 const config = reactive({
-    imgs: <string[]>[Z1, Z2, Z3, Z4, Z5, Z6, Z7, Z8, Z9, Z10, Z11, Z12, Z13, Z14, Z15, Z16, Z17, Z18, Z19, Z20, Z21, Z22, Z23, Z24, Z25, Z26, Z27, Z28, Z29, Z30, Z31, Z32, Z33, Z34, Z35, Z36, Z37, Z38, Z39, Z40, Z41, Z42, Z43, Z44, Z45, Z46, Z47, Z48, Z49, Z50, Z51, Z52, Z53, Z54, Z55, Z56, Z57, Z58, Z59, Z60, Z61, Z62, Z63, Z64, Z65, Z66, Z67, Z68, Z69, Z70, Z71, Z72, Z73, Z74, Z75, Z76, Z77, Z78, Z79, Z80, Z81, Z82, Z83]
+    imgs: <string[]>[Z01, Z02, Z03, Z04, Z05, Z06, Z08, Z09, Z10, Z11, Z12, Z13, Z14, Z15, Z16, Z17, Z19, Z20, Z21, Z22, Z23, Z24, Z25, Z27, Z28, Z29, Z30, Z31, Z32, Z33, Z34, Z35, Z36, Z37, Z38, Z39, Z40, Z41, Z42, Z43, Z44, Z45, Z46, Z47, Z48, Z49, Z50, Z51, Z52, Z53, Z54, Z55, Z56, Z57, Z58, Z59, Z60, Z61, Z62, Z63, Z64, Z65, Z66, Z67, Z68, Z69, Z70, Z71, Z72, Z73, Z74, Z75, Z76, Z77, Z78, Z79, Z80, Z81, Z82, Z83, Z84, Z85, Z86, Z87, Z88, Z89, Z90, Z91, Z92, Z93, Z94, Z95, Z96]
 })
+
+const scrollChange = () => {
+    const top: number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    scrollTop.value = top;
+}
+
+onMounted(() => {
+    window.addEventListener('scroll', scrollChange, true);
+})
+
+onUnmounted(() => {
+    window.removeEventListener('scroll', scrollChange, true);
+});
 </script>
 
 <style lang="scss" scoped>
 .body {
     $gap: 20px;
-    padding: $gap;
-    padding-top: 0;
     min-height: 100%;
     background-color: var(--t-background-color);
     display: flex;
@@ -138,42 +160,53 @@ const config = reactive({
     .header {
         position: sticky;
         top: 0;
-        padding: 8px;
+        padding: 8px 16px;
         display: flex;
+        align-items: center;
         justify-content: space-between;
         background-color: var(--t-background-color);
-        transition: background-color .25s;
+        transition: background-color .25s, box-shadow .25s;
         z-index: var(--nav-z-index);
 
-        .logo {
-            color: var(--red);
-            flex-shrink: 0;
-            display: flex;
-            align-items: center;
-        }
-
         .title {
+            padding-left: $gap;
             flex-grow: 1;
             display: flex;
             align-items: center;
-            justify-content: center;
+            justify-content: flex-start;
+            gap: 4px;
             font-size: 32px;
             font-weight: 600;
-            letter-spacing: 2px;
-            font-family: "Staatliches";
+            font-family: "Caveat";
+            letter-spacing: 6px;
+            transition: color .25s;
+
+            .icon {
+                margin-top: 8px;
+                color: var(--red);
+                flex-shrink: 0;
+                width: 36px;
+                height: 36px;
+            }
         }
 
         .theme-box {
+            min-width: 120px;
             flex-shrink: 0;
             display: flex;
             align-items: center;
             font-size: 14px;
-            color: var(--t-text-color-2);
+            color: var(--t-text-color-1);
         }
     }
 
+    .header.scroll {
+        box-shadow: var(--box-shadow);
+    }
+
     .group {
-        $padding: 10px;
+        $padding: 8px;
+        padding: $padding;
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
@@ -183,30 +216,39 @@ const config = reactive({
         gap: $gap;
 
         .item {
+            $spacing: 14px;
             position: relative;
             padding: $padding;
             display: flex;
             background-color: var(--t-card-background-color);
-            border-radius: 4px;
+            border-radius: 8px;
             box-shadow: var(--box-shadow);
             transition: background-color .25s;
 
             .index {
                 position: absolute;
-                top: calc($padding * 2);
-                right: calc($padding * 2);
-                padding: 4px 6px;
+                top: $spacing;
+                right: $spacing;
+                padding: 3px 6px;
                 font-size: 12px;
-                color: var(--white);
-                background-color: var(--black);
+                color: var(--t-text-color);
+                background-color: var(--t-card-background-color);
                 border-radius: 4px;
+                box-shadow: var(--box-shadow);
+                transition: color .25s, background-color .25s;
             }
 
             .img {
                 margin: 0;
+                min-width: 100px;
+                min-height: 100px;
                 max-width: 500px;
                 max-height: 420px;
                 border-radius: 4px;
+                -webkit-user-drag: none;
+                -webkit-user-select: none;
+                user-select: none;
+                pointer-events: none;
             }
         }
     }
--- vite.config.ts
+++ vite.config.ts
@@ -15,11 +15,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, __dirname);
   return {
     base: "/",
-    resolve: {
-      alias: {
-        "@": resolve(__dirname, "src"),
-      },
-    },
     plugins: [
       vue(),
       createHtmlPlugin({
@@ -28,7 +23,15 @@ export default defineConfig(({ mode }) => {
             title: env.VITE_APP_Title
           }
         }
-      })],
+      })
+    ],
+    // ******resolver配置******
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, './src'),
+      }
+    },
+    // ******开发服务器配置******
     server: {
       https: false,
       host: "0.0.0.0",
@@ -42,6 +45,28 @@ export default defineConfig(({ mode }) => {
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
       }
-    }
+    },
+    // ******css预处理器******
+    css: {
+      preprocessorOptions: {
+        less: {
+          modifyVars: {
+            // 全局less变量存储路径(配置less的全局变量)
+            hack: `true; @import (reference) "${resolve('src/public/config.less')}";`,
+          },
+          javascriptEnabled: true,
+        }
+      }
+    },
+    // ******项目构建配置******
+    build: {
+      target: 'modules', //设置最终构建的浏览器兼容目标  //es2015(编译成es5) | modules
+      outDir: 'dist', // 构建得包名  默认：dist
+      assetsDir: 'assets', // 静态资源得存放路径文件名  assets
+      sourcemap: false, //构建后是否生成 source map 文件
+      minify: 'esbuild', // 项目压缩 :boolean | 'terser' | 'esbuild'
+      chunkSizeWarningLimit: 1000, //chunk 大小警告的限制（以 kbs 为单位）默认：500
+      cssTarget: 'chrome61' //防止 vite 将 rgba() 颜色转化为 #RGBA 十六进制符号的形式  (要兼容的场景是安卓微信中的 webview 时,它不支持 CSS 中的 #RGBA 十六进制颜色符号)
+    },
   }
 });
